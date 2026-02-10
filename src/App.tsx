4f9aad7652ce115144b5849cb84cde80944f9673
강세아 <gangsea817@gmail.com>
2026-02-10 09:10:07 +0900

    fix: 관리자의 경로 수정 및 추가
    - 교직원 목록 페이지 경로 수정
    - 교직원 등록 페이지 경로 추가
    - 신청자 목록 페이지 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import "./index.css";
 import TeacherListPage from "./pages/admin/TeacherListPage";
+import TeacherCreatePage from "./pages/admin/TeacherCreatePage";
+import ApplicationListPage from "./pages/admin/ApplicationListPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<TeacherListPage />} />
-      <Route />
+      <Route path="/get/teacher" element={<TeacherListPage />} />
+      <Route path="/post/teacherCreate" element={<TeacherCreatePage />} />
+      <Route path="/get/apply" element={<ApplicationListPage />} />
     </Routes>
   );
 }
